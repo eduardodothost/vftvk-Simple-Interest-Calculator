f214f153eb2e0d79ee6a50012b49249375588ce2
Eduardo Rodrigues <eduardo@eduardo.host>
2022-06-26 21:58:54 -0400

    Updated code, ready to send to a PR

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100.00;
+    var years = new Date().getFullYear()+parseInt(years);
+    /*Send the interest calculation to the result span */
+    document.getElementById("result").innerText=
+    "If you deposit "+toUSD(principal)+
+    "\nat an interest rate of "+rate+"%"+ 
+    "\nYou will recieve an amount of "+
+    toUSD(interest) +
+    ",\n in the year "+years;
+
+   
     
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+     /*Send the Rate input to the rate_val span */
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+function toUSD(money) {
+    var value = new Intl.NumberFormat('en-US', 
+    { style: 'currency', currency: 'USD' } 
+  ).format(money);
+        return value;
+       
+}
